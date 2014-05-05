cfc85956b57dfdcbd754628536ef0c85187446cb
Anthony Maladot <anthony.maladot@gmail.com>
2014-05-05 18:47:05 +0200

    Ajout image liste calendrier

--- src/com/hofc/hofc/adapter/CalendrierAdapter.java
+++ src/com/hofc/hofc/adapter/CalendrierAdapter.java
@@ -1,6 +1,7 @@
 package com.hofc.hofc.adapter;
 
 import com.hofc.hofc.R;
+import com.hofc.hofc.constant.AppConstant;
 import com.hofc.hofc.data.DataSingleton;
 
 import android.content.Context;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class CalendrierAdapter extends BaseAdapter {
@@ -38,17 +40,22 @@ public class CalendrierAdapter extends BaseAdapter {
 		if(convertView == null) {
 			holder = new ViewHolder();
 			convertView = inflater.inflate(R.layout.item_calendrier, null);
-			
+			holder.imageEquipe1 = (ImageView)convertView.findViewById(R.id.calendrier_image_1);
 			holder.calendrierEquipe1 = (TextView)convertView.findViewById(R.id.calendrier_equipe_1);
 			holder.calendrierScore1 = (TextView)convertView.findViewById(R.id.calendrier_score_1);
 			holder.calendrierScore2 = (TextView)convertView.findViewById(R.id.calendrier_score_2);
 			holder.calendrierEquipe2 = (TextView)convertView.findViewById(R.id.calendrier_equipe_2);
+			holder.imageEquipe2 = (ImageView)convertView.findViewById(R.id.calendrier_image_2);
 			
 			convertView.setTag(holder);
 		} else {
 			holder = (ViewHolder) convertView.getTag();
 		}
-		
+		if(AppConstant.hofcName.equalsIgnoreCase(DataSingleton.getCalendrier().get(position).getEquipe1())) {
+			holder.imageEquipe1.setImageResource(R.drawable.ic_launcher);
+		} else if(AppConstant.hofcName.equalsIgnoreCase(DataSingleton.getCalendrier().get(position).getEquipe2())) {
+			holder.imageEquipe2.setImageResource(R.drawable.ic_launcher);
+		}
 		holder.calendrierEquipe1.setText(DataSingleton.getCalendrier().get(position).getEquipe1());
 		holder.calendrierScore1.setText(DataSingleton.getCalendrier().get(position).getScore1()+"");
 		holder.calendrierScore2.setText(DataSingleton.getCalendrier().get(position).getScore2()+"");
@@ -58,10 +65,12 @@ public class CalendrierAdapter extends BaseAdapter {
 	}
 	
 	private class ViewHolder {
+		ImageView imageEquipe1;
 		TextView calendrierEquipe1;
 		TextView calendrierScore1;
 		TextView calendrierScore2;
 		TextView calendrierEquipe2;
+		ImageView imageEquipe2;
 	}
 
 }
---
+++ src/com/hofc/hofc/constant/AppConstant.java
@@ -0,0 +1,5 @@
+package com.hofc.hofc.constant;
+
+public class AppConstant {
+	public static final String hofcName = "HORGUES ODOS F.C.";
+}
