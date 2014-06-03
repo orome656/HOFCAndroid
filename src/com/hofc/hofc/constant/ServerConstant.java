b3cb7de8fecb0cee18dff7993509504fcf4dcffc
Orome656 <anthony.maladot@gmail.com>
2014-06-03 22:21:05 +0200

    Gitignore

--- src/com/hofc/hofc/constant/ServerConstant.java
+++ src/com/hofc/hofc/constant/ServerConstant.java
@@ -2,7 +2,9 @@ package com.hofc.hofc.constant;
 
 public class ServerConstant {
 	
-	public static final String SERVER_URL = "";
+	public static final String SERVER_URL = "orome656.dynamic-dns.net";
+	//public static final String SERVER_URL = "192.168.1.50";
+	//public static final String SERVER_URL = "192.168.137.1";
 	public static final int SERVER_PORT = 3000;
 	public static final String CALENDRIER_CONTEXT = "calendrier";
 	public static final String CLASSEMENT_CONTEXT = "classement";
