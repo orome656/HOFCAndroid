ebbd129d918fc548a09929e1bbb51018dac84a03
Orome656 <anthony.maladot@gmail.com>
2014-07-10 13:54:52 +0200

    Add click listener to open article on the web

--- src/com/hofc/hofc/adapter/ActusAdapter.java
+++ src/com/hofc/hofc/adapter/ActusAdapter.java
@@ -1,6 +1,11 @@
 package com.hofc.hofc.adapter;
 
+import java.net.URL;
+
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.sax.StartElementListener;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -50,6 +55,7 @@ public class ActusAdapter extends BaseAdapter {
 			holder.titleText = (TextView)convertView.findViewById(R.id.actus_view_title);
 			holder.texte = (TextView)convertView.findViewById(R.id.actus_view_texte);
 			holder.dateView = (TextView)convertView.findViewById(R.id.actus_view_date);
+			holder.urlActus = (TextView)convertView.findViewById(R.id.actus_url);
 			convertView.setTag(holder);
 		} else {
 			holder = (ViewHolder) convertView.getTag();
@@ -65,8 +71,19 @@ public class ActusAdapter extends BaseAdapter {
 		holder.titleText.setText(line.getTitre());
 		holder.texte.setText(line.getTexte());
 		holder.dateView.setText(line.getDate().toString());
+		holder.urlActus.setText(line.getUrl());
 		
 		//Ajouter au clic l'ouverture de l'url 
+		convertView.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				String url = ((TextView)v.findViewById(R.id.actus_url)).getText().toString();
+				Intent i = new Intent(Intent.ACTION_VIEW);
+				i.setData(Uri.parse(url));
+				v.getContext().startActivity(i);
+			}
+		});
 		
 		return convertView;
 	}
@@ -76,6 +93,7 @@ public class ActusAdapter extends BaseAdapter {
 		TextView titleText;
 		TextView texte;
 		TextView dateView;
+		TextView urlActus;
 	}
 
 }
