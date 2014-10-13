77759c7b08634b819749d6524bd75fd3c303090f
Orome656 <anthony.maladot@gmail.com>
2014-10-13 20:58:57 +0200

    Android Studio Migration

---
+++ app/src/main/java/com/hofc/hofc/constant/ServerConstant.java
@@ -0,0 +1,14 @@
+package com.hofc.hofc.constant;
+
+public class ServerConstant {
+	
+	public static final String SERVER_URL = "orome656.dynamic-dns.net";
+	//public static final String SERVER_URL = "192.168.1.81";
+	//public static final String SERVER_URL = "192.168.137.1";
+	public static final int SERVER_PORT = 3000;
+	public static final String CALENDRIER_CONTEXT = "calendrier";
+	public static final String CLASSEMENT_CONTEXT = "classement";
+	public static final String ACTUS_CONTEXT = "actus";
+	
+	public static final int NOMBRE_JOUR_SYNCHRO = 7;
+}
