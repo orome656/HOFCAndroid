0a84ccfd84afefde4665bf43bce37ffacabcedf3
Orome656 <anthony.maladot@gmail.com>
2015-02-13 13:40:33 +0100

    Add custom application class

---
+++ app/src/main/java/com/hofc/hofc/HOFCApplication.java
@@ -0,0 +1,33 @@
+package com.hofc.hofc;
+
+import android.app.Application;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by maladota on 13/02/2015.
+ */
+public class HOFCApplication extends Application {
+    private RequestQueue requestQueue;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        init();
+    }
+
+    @Override
+    public void onTerminate() {
+        requestQueue.stop();
+        super.onTerminate();
+    }
+
+    private void init() {
+        requestQueue = Volley.newRequestQueue(this);
+    }
+
+    public RequestQueue getRequestQueue() {
+        return requestQueue;
+    }
+
+}
