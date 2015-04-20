376a4c1379a73253a8cad31b960ce023e011d984
Orome656 <anthony.maladot@gmail.com>
2015-04-20 18:11:59 +0200

    Inject properties at build time

--- app/src/main/java/com/hofc/hofc/constant/ServerConstant.java
+++ app/src/main/java/com/hofc/hofc/constant/ServerConstant.java
@@ -1,15 +1,17 @@
 package com.hofc.hofc.constant;
 
+import com.hofc.hofc.BuildConfig;
+
 public class ServerConstant {
-	public static String SERVER_URL_PREFIX = "https://";
-	public static String SERVER_URL = "quiet-wave-7010.herokuapp.com";
-    public static int SERVER_PORT = 0;
+	public static String SERVER_URL_PREFIX = BuildConfig.SERVER_URL_PREFIX;
+	public static String SERVER_URL = BuildConfig.SERVER_URL;
+    public static int SERVER_PORT = BuildConfig.SERVER_PORT;
 	public static final String CALENDRIER_CONTEXT = "calendrier";
 	public static final String CLASSEMENT_CONTEXT = "classement";
 	public static final String ACTUS_CONTEXT = "actus";
     public static final String NOTIF_CONTEXT = "registerPush";
     public static final String PARSE_PAGE_CONTEXT = "parsePage";
-    public static final String AGENDA_CONTEXT = "agenda";
-	public static final String MATCH_CONTEXT = "matchinfos";
+    public static final String AGENDA_CONTEXT = "agendadistrict";
+	public static final String MATCH_CONTEXT = "matchinfosdistrict";
 	public static final int NOMBRE_JOUR_SYNCHRO = 7;
 }
