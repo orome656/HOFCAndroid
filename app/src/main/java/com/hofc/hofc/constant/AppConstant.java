257e8fdb118514ef61cbf2722c396312bff062e2
Orome656 <anthony.maladot@gmail.com>
2015-04-21 13:40:33 +0200

    Change Build Task

--- app/src/main/java/com/hofc/hofc/constant/AppConstant.java
+++ app/src/main/java/com/hofc/hofc/constant/AppConstant.java
@@ -1,7 +1,9 @@
 package com.hofc.hofc.constant;
 
+import com.hofc.hofc.BuildConfig;
+
 public class AppConstant {
 	public static final String hofcName = "HORGUES ODOS F.C.";
     public static final int AGENDA_WEEK_NUMBER = 3;
-    public static final String SENDER_ID = "";
+    public static final String SENDER_ID = BuildConfig.SENDER_ID;
 }
